f7bd41118ed4f3ffb6b2d87997d29dd3a061dc84
george ebeh <georgeebeh@gmail.com>
2023-04-23 11:05:21 +0100

    added variables to branch

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
-  region  = "us-central1"
-  zone    = "us-central1-a"
+  project = var.project-id #replace this with your project-id
+  region  = var.region 
+  zone    = var.zone 
 }
 
 ### NETWORK
@@ -12,10 +12,10 @@ data "google_compute_network" "default" {
 
 ## SUBNET
 resource "google_compute_subnetwork" "subnet-1" {
-  name                     = "subnet1"
-  ip_cidr_range            = "10.127.0.0/20"
+  name                     = var.subnet-name 
+  ip_cidr_range            = var.subnet-cidr 
   network                  = data.google_compute_network.default.self_link
-  region                   = "us-central1"
+  region                   = var.region 
   private_ip_google_access = true
 }
 
